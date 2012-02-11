ee2cf932fd013dbc7c9cc9e06c1a474afd286fbc
Kalashnikov <kalashnikov+git@opentomsk.net>
2012-02-11 22:09:57 +0700

    rm old folder, backup tiers.xml

--- rpc scripts/scripts/storage.js
+++
@@ -1,9 +0,0 @@
-function Storage(name) {
-	this.filename = name;
-	this.values = {};
-}
-
-Storage.prototype.add = function(key, value)
-Storage.prototype.get = function(key)
-Storage.prototype.delete = function(key)
-Storage.prototype.load = function()
--- rpc scripts/scripts/user.js
+++
@@ -1,8 +0,0 @@
-function User(id) {
-	this.id = id;
-	this.muted = false;
-}
-
-User.prototype.foo = 'bar';
-
-User // for eval
